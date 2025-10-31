c9b53bfe7584c350488127c99bb1e14890078e5b
Ersin Isgor <ersinisgor@hotmail.com>
2025-10-31 16:53:52 +0300

    feat(be-config): add environment variable validation schema

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -5,12 +5,14 @@ import { ConfigModule } from '@nestjs/config';
 import { UserModule } from './user/user.module';
 import { PrismaModule } from './prisma/prisma.module';
 import configuration from './config/configuration';
+import validationSchema from './config/validation';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
       load: [configuration],
+      validationSchema,
     }),
     UserModule,
     PrismaModule,
