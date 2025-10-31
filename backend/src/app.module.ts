d8b20a769ded0117a7e1ba85f7c8dc164a1e15d1
Ersin Isgor <ersinisgor@hotmail.com>
2025-10-31 16:43:23 +0300

    feat(be-config): add configuration service with environment variables

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -4,11 +4,13 @@ import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { UserModule } from './user/user.module';
 import { PrismaModule } from './prisma/prisma.module';
+import configuration from './config/configuration';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
+      load: [configuration],
     }),
     UserModule,
     PrismaModule,
