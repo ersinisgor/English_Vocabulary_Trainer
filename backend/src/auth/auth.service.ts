3e5f478e7d676fcbeaa5dba7d1a498614bbcaf58
Ersin Isgor <ersinisgor@hotmail.com>
2025-11-06 05:52:31 +0300

    feat(be-auth): implement login functionality to auth service with JWT authentication

--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -1,4 +1,41 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { UsersService } from 'src/users/users.service';
+import { LoginDTO } from './dtos/login.dto';
+import { LoginResponseDTO } from './dtos/login-response.dto';
+import * as bcrypt from 'bcrypt';
+import { JwtService } from '@nestjs/jwt';
+import { JwtPayload } from './types/interfaces/jwt-payload.interface';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    private usersService: UsersService,
+    private jwtService: JwtService,
+  ) {}
+
+  async login(loginDTO: LoginDTO): Promise<LoginResponseDTO> {
+    const { password, email } = loginDTO;
+
+    const user = await this.usersService.findOneByEmail(email);
+    if (!user) throw new UnauthorizedException('Invalid credentials');
+
+    const isPasswordValid = await bcrypt.compare(password, user.passwordHash);
+    if (!isPasswordValid)
+      throw new UnauthorizedException('Invalid credentials');
+
+    const payload: JwtPayload = {
+      sub: user.id,
+      email: user.email,
+    };
+
+    const token = await this.generateToken(payload);
+
+    return token;
+  }
+
+  private async generateToken(payload: JwtPayload): Promise<LoginResponseDTO> {
+    return {
+      accessToken: await this.jwtService.signAsync(payload),
+    };
+  }
+}
