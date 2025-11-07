bb604cf9bc7ad6ca3c92af9644a2fef3bd4ed377
Ersin Isgor <ersinisgor@hotmail.com>
2025-11-07 17:59:44 +0300

    refactor(be-users): remove validation decorators from create-user dto

--- backend/src/users/dtos/create-user.dto.ts
+++ backend/src/users/dtos/create-user.dto.ts
@@ -1,14 +1,5 @@
-import { IsEmail, IsOptional, IsString, MinLength } from 'class-validator';
-
 export class CreateUserDto {
-  @IsEmail()
   email: string;
-
-  @IsOptional()
-  @IsString()
   username?: string;
-
-  @IsString()
-  @MinLength(8)
   password: string;
 }
