c589d8786007e4eadc31dce2f1cf1398b285b13c
Ersin Isgor <ersinisgor@hotmail.com>
2025-11-26 07:08:19 +0300

    docs(be-users): add swagger api documentation for user response

--- backend/src/users/dtos/user-response.dto.ts
+++ backend/src/users/dtos/user-response.dto.ts
@@ -1,19 +1,28 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { Exclude, Expose } from 'class-transformer';
 
 @Exclude()
 export class UserResponseDto {
   @Expose()
+  @ApiProperty({ example: 'clu278fem0000c9nbwq3x7m7v' })
   id: string;
 
   @Expose()
+  @ApiProperty({ example: 'john@example.com' })
   email: string;
 
   @Expose()
+  @ApiProperty({
+    nullable: true,
+    example: 'johnny',
+  })
   username: string | null;
 
   @Expose()
+  @ApiProperty({ example: '2025-01-01T12:00:00.000Z' })
   createdAt: Date;
 
   @Expose()
+  @ApiProperty({ example: '2025-01-02T14:20:00.000Z' })
   updatedAt: Date;
 }
