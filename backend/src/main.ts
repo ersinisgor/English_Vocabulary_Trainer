41dd189f0d8e8b39db3ae0953cf9a51239a15c48
Ersin Isgor <ersinisgor@hotmail.com>
2025-11-26 05:04:50 +0300

    feat(beckend): add cookie parser and improve error handling in bootstrap

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,11 +4,13 @@ import { Logger, ValidationPipe } from '@nestjs/common';
 import { HttpExceptionFilter } from './common/filters/http-exception.filter';
 import { ConfigService } from '@nestjs/config';
 import { TransformInterceptor } from './common/interceptors/transform.interceptor';
+import cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const logger = new Logger('Bootstrap');
 
+  app.use(cookieParser());
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true, // strips unknown properties
@@ -20,9 +22,11 @@ async function bootstrap() {
   app.useGlobalInterceptors(new TransformInterceptor());
 
   const config = app.get(ConfigService);
-  const port = config.get<number>('PORT') ?? 3000;
+  const port = config.get<number>('port') ?? 3000;
 
   await app.listen(port);
   logger.log(`Application listening on port ${port}`);
 }
-bootstrap();
+bootstrap().catch((err) => {
+  console.error('Failed to start application: ', err);
+});
