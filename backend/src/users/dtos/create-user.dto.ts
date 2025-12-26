e632ec6d2ed747e64804641fd7b2fa2198a16789
Ersin Isgor <ersinisgor@hotmail.com>
2025-12-26 10:05:33 +0300

    feat(be-users): add role field and validation to user DTOs

--- backend/src/users/dtos/create-user.dto.ts
+++ backend/src/users/dtos/create-user.dto.ts
@@ -1,5 +1,40 @@
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
+import {
+  IsEmail,
+  IsOptional,
+  IsString,
+  MinLength,
+  MaxLength,
+  IsEnum,
+} from 'class-validator';
+import { Role } from 'generated/prisma';
+
 export class CreateUserDTO {
+  @ApiProperty({
+    example: 'john@example.com',
+    description: 'Unique email address of the user',
+  })
+  @IsEmail()
   email: string;
+
+  @ApiPropertyOptional({
+    example: 'johnny',
+    description: 'Optional username',
+  })
+  @IsOptional()
+  @IsString()
+  @MaxLength(30)
   username?: string;
+
+  @ApiProperty({
+    example: 'StrongP@ssw0rd',
+    minLength: 8,
+  })
+  @IsString()
+  @MinLength(8)
   password: string;
+
+  @IsOptional()
+  @IsEnum(Role)
+  role?: Role;
 }
--- backend/src/users/dtos/user-response.dto.ts
+++ backend/src/users/dtos/user-response.dto.ts
@@ -1,5 +1,6 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { Exclude, Expose } from 'class-transformer';
+import { Role } from 'generated/prisma';
 
 @Exclude()
 export class UserResponseDto {
@@ -17,6 +18,12 @@ export class UserResponseDto {
     example: 'johnny',
   })
   username: string | null;
+
+  @Expose()
+  @ApiProperty({
+    nullable: true,
+  })
+  role: Role | null;
 
   @Expose()
   @ApiProperty({ example: '2025-01-01T12:00:00.000Z' })
