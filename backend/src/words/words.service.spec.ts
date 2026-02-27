199aa6e7d4c64b6e2a0f2117236d702c57f0615a
Ersin Isgor <ersinisgor@hotmail.com>
2026-02-27 14:48:39 +0300

    test(be-words): update unit tests to include meaningOrder in WordsController and WordsService

--- backend/src/words/words.service.spec.ts
+++ backend/src/words/words.service.spec.ts
@@ -29,6 +29,7 @@ describe('WordsService', () => {
     word: 'test',
     level: WordLevel.A1,
     partOfSpeech: PartOfSpeech.NOUN,
+    meaningOrder: 1,
     userId,
     createdAt: new Date(),
     updatedAt: new Date(),
