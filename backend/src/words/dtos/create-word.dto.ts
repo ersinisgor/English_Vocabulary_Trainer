ff49bc4ac1b98e8a3982765ba03d6d2f57f1d0d0
Ersin Isgor <ersinisgor@hotmail.com>
2026-02-28 21:36:47 +0300

    refactor(be-words): clean up CreateWordDTO by removing meaningOrder property

--- backend/src/words/dtos/create-word.dto.ts
+++ backend/src/words/dtos/create-word.dto.ts
@@ -1,11 +1,5 @@
 import { PartOfSpeech, WordLevel } from 'generated/prisma';
-import {
-  IsEnum,
-  IsNotEmpty,
-  IsNumber,
-  IsOptional,
-  IsString,
-} from 'class-validator';
+import { IsEnum, IsNotEmpty, IsOptional, IsString } from 'class-validator';
 
 export class CreateWordDTO {
   @IsString()
@@ -18,8 +12,4 @@ export class CreateWordDTO {
 
   @IsEnum(PartOfSpeech)
   partOfSpeech: PartOfSpeech;
-
-  @IsOptional()
-  @IsNumber()
-  meaningOrder?: number;
 }
